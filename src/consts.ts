08338f799ed02897a173488db4db039b9925cb1f
didymu5 <wu.thomas@gmail.com>
2024-08-26 14:22:18 -0700

    feat: first big commit

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Christ-Centered Spirituality";
+export const SITE_DESCRIPTION =
+  "This is an accompaniment to the guided Christ-Centered Spirituality authored by Joel Stephen Elies, D.Min";
